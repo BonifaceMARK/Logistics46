c8a991ec96eae182ea40e932e13856f8d7b164c0
Mark Luis <markluisbonifacio@gmail.com>
2024-03-19 11:35:21 +0800

    first commit

---
+++ routes/web.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\language\LanguageController;
+use App\Http\Controllers\pages\HomePage;
+use App\Http\Controllers\pages\Page2;
+use App\Http\Controllers\pages\MiscError;
+use App\Http\Controllers\authentications\LoginBasic;
+use App\Http\Controllers\authentications\RegisterBasic;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+//Redirect to login page
+Route::redirect(uri: '/', destination: 'login');
+
+// locale
+Route::get('lang/{locale}', [LanguageController::class, 'swap']);
+
+Route::middleware([
+  'auth:sanctum',
+  config('jetstream.auth_session'),
+  'verified',
+])->group(function () {
+  Route::get('/dashboard', function () {
+    return view('dashboard');
+  })->name('dashboard');
+
+  // Main Page Route
+  Route::get('/', [HomePage::class, 'index'])->name('pages-home');
+  Route::get('/page-2', [Page2::class, 'index'])->name('pages-page-2');
+});
